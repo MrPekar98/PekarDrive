ad9e8513bf3db991e97dab3be6c72eacc02f4562
MrPekar98 <martin.pekar@outlook.dk>
2020-12-14 21:02:20 +0100

    Added for-each loop of server table.

--- src/server_table.c
+++ src/server_table.c
@@ -1,1 +1,29 @@
 #include "server_table.h"
+#include <stdlib.h>
+
+static struct file_server *server_table = NULL;
+static unsigned short server_count = 0;
+
+// Adds server to table.
+void add_server(struct file_server server)
+{
+    server_table = (struct file_server *) realloc(server_table, sizeof(struct file_server) * ++server_count);
+    server_table[server_count - 1] = server;
+}
+
+// Removed file server from table.
+void remove_server(ftid_t id)
+{
+
+}
+
+// For-each loop of server table.
+void for_each_server(void (*do)(struct fileserver *))
+{
+    unsigned i;
+
+    for (i = 0; i < server_count; i++)
+    {
+        do(&server_table[i]);
+    }
+}
--- src/server_table.h
+++ src/server_table.h
@@ -1,9 +1,12 @@
 #ifndef SERVER_TABLE_H
 #define SERVER_TABLE_H
 
+typedef unsigned long ftid_t;
+
 // Defines file server.
 struct file_server
 {
+    ftid_t id;
     long size;
 
     struct
@@ -12,5 +15,11 @@ struct file_server
         unsigned short port;
     } location;
 };
+
+// Prototypes arguments are this way to ensure that added servers are never changed.
+// If one is to change a server, it must be removed and then re-added.
+void add_server(struct file_server server);
+void remove_server(ftid_t id);
+void for_each_server(void (*do)(struct fileserver *));
 
 #endif
