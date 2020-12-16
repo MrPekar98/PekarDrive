77b26e81864dee3f13bfbc696845b15db0db573c
MrPekar98 <martin.pekar@outlook.dk>
2020-12-16 22:31:47 +0100

    New master file.

---
+++ src/master.c
@@ -0,0 +1,14 @@
+#include <pthread.h>
+
+// Stores information about workers.
+// Load balances workload based in worker information in server table.
+// When client wants to read a file, find the file amongst the workers. This is also true when appending to a file.
+int main()
+{
+    while (1)
+    {
+
+    }
+
+    return 0;
+}
