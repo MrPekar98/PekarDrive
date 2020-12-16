fce52f0ffc7bd7e02e2ea0ef28ace9bd122d03a7
MrPekar98 <martin.pekar@outlook.dk>
2020-12-16 22:55:54 +0100

    Designed architecture.

--- src/server.c
+++ src/server.c
@@ -3,17 +3,50 @@
 #include <comm.h>
 #include "server_table.h"
 #include <stdlib.h>
+#include <pthread.h>
 #include <sys/socket.h>
-#include <unistd.h>
+
+void *handle_client(void *arg);
 
 int main()
 {
-    // Implement locking so that no two clients access the same file at once. No shared locks.
+    // TODO: Implement locking so that no two clients access the same file at once. No shared locks.
+    // TODO: Add to /lib/ a packet format file that specifies message format. This must be included in interface.h.
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (!server_fd)
+    {
+#ifdef LOG
+        printf("Connection setup failed.\n");
+#endif
+        return 1;
+    }
 
     while (1)
     {
+        pthread_t thread;
+        conn client = conn_listen(server_fd);
 
+        if (client.error)
+        {
+#ifdef LOG
+            printf("Client connection refused.\n");
+#endif
+            continue;
+        }
+
+        pthread_create(&thread, NULL, handle_client, &client);
     }
 
     return 0;
+}
+
+// Client handler.
+void *handle_client(void *arg)
+{
+    conn client = *((conn *) arg);
+
+    pthread_exit(NULL);
+    return NULL;
 }
