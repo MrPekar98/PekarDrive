e79a63e4a4e7c41e848079577bb919dc0d6eacf2
MrPekar98 <martin.pekar@outlook.dk>
2020-12-23 17:47:59 +0100

    Preprocessing error fix.

--- src/server/server.c
+++ src/server/server.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "fs/fs.h"
+#include "../fs/fs.h"
 #include <comm.h>
-#include "server_table.h"
+#include "../server_table.h"
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/socket.h>
