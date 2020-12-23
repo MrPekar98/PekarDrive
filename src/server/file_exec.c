1ec6f4ae0e19e7bae40911adefe5cec7621dc293
MrPekar98 <martin.pekar@outlook.dk>
2020-12-23 20:36:17 +0100

    Executes operations on files.

---
+++ src/server/file_exec.c
@@ -0,0 +1,22 @@
+#include "file_exec.c"
+#include <pthread.h>
+
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Entrypoint for handling file operations.
+struct file_output f_exec(short op, const void *arg, size_t len)
+{
+    pthread_mutex_lock(&mutex);
+
+    if (op & FILE_READ)
+    {
+
+    }
+
+    if (op & FILE_WRITE)
+    {
+
+    }
+
+    pthread_mutex_unlock(&mutex);
+}
---
+++ src/server/file_exec.h
@@ -0,0 +1,15 @@
+#ifndef FILE_EXEC_H
+#define FILE_EXEC_H
+
+#define FILE_READ 1
+#define FILE_WRITE 2
+
+struct file_output
+{
+    void *out;
+    unsigned len;
+};
+
+struct file_output f_exec(short op, const void *arg, size_t len);
+
+#endif
