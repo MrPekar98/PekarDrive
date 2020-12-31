0f41ed990d41dbc3062e6206fc6338f7d22ebe74
MrPekar98 <martin.pekar@outlook.dk>
2020-12-31 14:53:22 +0100

    Uses administrator.

--- src/master.c
+++ src/master.c
@@ -1,10 +1,13 @@
 #include <pthread.h>
+#include "worker_admin.h"
 
 // Stores information about workers.
 // Load balances workload based in worker information in server table.
 // When client wants to read a file, find the file amongst the workers. This is also true when appending to a file.
 int main()
 {
+    pthread_t admin = start_admin();
+
     while (1)
     {
 
