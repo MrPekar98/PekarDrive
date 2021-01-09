04153a52f0f6949f54ca9ee476fa7993fae48131
MrPekar98 <martin.pekar@outlook.dk>
2021-01-09 16:05:54 +0100

    Small declaration improvements.

--- src/fs/fs.h
+++ src/fs/fs.h
@@ -8,7 +8,7 @@ extern "C"
 
 short fs_create_file(const char *file);
 void fs_delete_file(const char *file);
-long fs_write_file(const char *file, void *buffer, unsigned long size, short isappend);
+long fs_write_file(const char *file, const void *buffer, unsigned long size, short isappend);
 void *fs_read_file(const char *file);
 
 #ifdef __cplusplus
