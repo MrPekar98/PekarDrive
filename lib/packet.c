116b6983f7018a3eeed3d50d91b34cef803f31bc
MrPekar98 <martin.pekar@outlook.dk>
2021-01-10 18:19:41 +0100

    Added token field.

--- lib/packet.c
+++ lib/packet.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 
 // Initializes packet struct.
-struct packet p_init(unsigned seq_number, const char *arg, enum type msg_type)
+struct packet p_init(unsigned seq_number, const char *arg, enum type msg_type, unsigned long token)
 {
     struct packet p = {.seq_number = seq_number, .arg = malloc(strlen(arg) + 1),
-                        .msg_len = strlen(arg), .msg_type = msg_type, .error = 0};
+                        .msg_len = strlen(arg), .msg_type = msg_type, .error = 0, .token = token};
     strcpy(p.arg, arg);
     return p;
 }
@@ -19,7 +19,8 @@ const void *p_encode(struct packet p)
     memcpy(buffer + sizeof(unsigned), &p.msg_len, sizeof(unsigned));
     memcpy(buffer + sizeof(int) * 2, &p.msg_type, sizeof(int));
     memcpy(buffer + sizeof(int) * 3, &p.error, sizeof(short));
-    memcpy(buffer + sizeof(int) * 3 + sizeof(short), p.arg, p.msg_len);
+    memcpy(buffer + sizeof(int) * 3 + sizeof(short), p.token, sizeof(unsigned long));
+    memcpy(buffer + sizeof(int) * 3 + sizeof(short) + sizeof(unsigned long), p.arg, p.msg_len);
 
     return buffer;
 }
@@ -32,9 +33,10 @@ struct packet p_decode(const void *p)
     memcpy(&res.msg_len, p + sizeof(unsigned), sizeof(unsigned));
     memcpy(&res.msg_type, p + sizeof(unsigned) * 2, sizeof(unsigned));
     memcpy(&res.error, p + sizeof(unsigned) * 3, sizeof(short));
+    memcpy(&res.token, p + sizeof(unsigned) * 3 + sizeof(short), sizeof(unsigned long))
 
     res.arg = malloc(res.msg_len);
-    memcpy(res.arg, p + sizeof(unsigned) * 3 + sizeof(short), res.msg_len);
+    memcpy(res.arg, p + sizeof(unsigned) * 3 + sizeof(short) + sizeof(unsigned long), res.msg_len);
 
     return res;
 }
@@ -46,10 +48,10 @@ void p_cleanup(struct packet p)
 }
 
 // Error packet.
-struct packet p_error(unsigned seq_number, const char *arg, enum type t)
+struct packet p_error(unsigned seq_number, const char *arg, enum type t, unsigned long token)
 {
     struct packet p = {.seq_number = seq_number, .arg = malloc(strlen(arg) + 1),
-                        .msg_len = strlen(arg), .msg_type = t, .error = 1};
+                        .msg_len = strlen(arg), .msg_type = t, .error = 1, .token = token};
     strcpy(p.arg, arg);
     return p;
 }
--- lib/packet.h
+++ lib/packet.h
@@ -7,6 +7,7 @@ struct packet
     char *arg;
     unsigned msg_len;
     unsigned short error;
+    unsigned long token;
 
     enum type
     {
@@ -22,8 +23,8 @@ struct packet
 
 const void *p_encode(struct packet p);
 struct packet p_decode(const void *p);
-struct packet p_init(unsigned seq_number, const char *arg, enum type t);
+struct packet p_init(unsigned seq_number, const char *arg, enum type t, unsigned long token);
 void p_cleanup(struct packet p);
-struct packet p_error(unsigned seq_number, const char *arg, enum type t);
+struct packet p_error(unsigned seq_number, const char *arg, enum type t, unsigned long token);
 
 #endif
