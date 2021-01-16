8e084495a039962c63deafd027e1d0525044c9e3
MrPekar98 <martin.pekar@outlook.dk>
2021-01-16 18:56:52 +0100

    Temporary implementation.

--- src/balance.c
+++ src/balance.c
@@ -1,1 +1,12 @@
 #include "balance.h"
+#include <stdlib.h>
+
+// Returns file_server with least work load.
+// Temporary implementation.
+struct file_server *least_load()
+{
+    if (table_count() == 0)
+        return NULL;
+
+    return getfs_index(0);
+}
--- src/balance.h
+++ src/balance.h
@@ -1,4 +1,8 @@
 #ifndef BALANCE_H
 #define BALANCE_H
 
+#include "server_table.h"
+
+struct file_server *least_load();
+
 #endif
