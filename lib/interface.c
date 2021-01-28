b81e4fc7f584f63f8f2703ad91765e770189363e
MrPekar98 <martin.pekar@outlook.dk>
2021-01-28 21:09:47 +0100

    Implemented missing operations.

--- lib/interface.c
+++ lib/interface.c
@@ -5,7 +5,32 @@
 #include <packet.h>
 #include <stdio.h>
 
-void *read_all(conn client);
+static void *read_all(conn client);
+static const char *gen_write_arg(const char *file, const char *data);
+
+// Executes 'ls' command.
+const void *ls(const char *host, unsigned short port)
+{
+    struct packet p = p_init(0, "", LS);
+    conn client = client_init(host, port);
+
+    if (client.error)
+    {
+#ifdef LOG
+        printf("%s\n", client.error_msg);
+#endif
+        return NULL;
+    }
+
+    conn_write(client, p_encode(p), sizeof(p) + 3);
+
+    void *buffer = read_all(client);
+    struct packet received = p_decode(buffer);
+    conn_close(&client);
+    free(buffer);
+
+    return !received.error ? received.arg : NULL;
+}
 
 // Reads all bytes of file.
 const void *file_read(const char *file_name, const char *host, unsigned short port)
@@ -22,14 +47,17 @@ const void *file_read(const char *file_name, const char *host, unsigned short po
     }
 
     conn_write(client, p_encode(p), sizeof(p) + strlen(file_name));
-    void *buffer = read_all(client);
-    conn_close(&client);
 
-    return p_decode(buffer).arg;
+    void *buffer = read_all(client);
+    struct packet received = p_decode(buffer);
+    conn_close(&client);
+    free(buffer);
+
+    return !received.error ? received.arg : NULL;
 }
 
 // Empties stream of bytes and returns it in a buffer.
-void *read_all(conn client)
+static void *read_all(conn client)
 {
     const unsigned short read_size = 1000;
     unsigned i = 0;
@@ -46,11 +74,56 @@ void *read_all(conn client)
 // Writes bytes to file.
 size_t file_write(const char *file_name, const void *buffer, size_t len, short append, const char *host, unsigned short port)
 {
-    return 0;
+    struct packet p = p_init(0, gen_write_arg(file_name, buffer), append ? APPEND : WRITE);
+    conn client = client_init(host, port);
+
+    if (client.error)
+    {
+#ifdef LOG
+        printf("%s\n", client.error_msg);
+#endif
+        return -1;
+    }
+
+    conn_write(client, p_encode(p), sizeof(p) + strlen(p.arg) + 1);
+
+    void *buffer_received = read_all(client);
+    struct packet received = p_decode(buffer_received);
+    conn_close(&client);
+    free(buffer_received);
+
+    return !received.error ? *(long *) received.arg : -1;
+}
+
+// Generates argument for write and append.
+static const char *gen_write_arg(const char *file, const char *data)
+{
+    char *arg = malloc(strlen(file) + strlen(data) + 3);
+    sprintf(arg, "%s;%s\0", file, data);
+
+    return arg;
 }
 
 // Deletes file if existing.
 short file_delete(const char *file_name, const char *host, unsigned short port)
 {
-    return 0;
+    struct packet p = p_init(0, file_name, DELETE);
+    conn client = client_init(host, port);
+
+    if (client.error)
+    {
+#ifdef LOG
+        printf("%s\n", client.error_msg);
+#endif
+        return 0;
+    }
+
+    conn_write(client, p_encode(p), sizeof(p) + strlen(p.arg) + 1);
+
+    void *buffer_received = read_all(client);
+    struct packet received = p_decode(buffer_received);
+    conn_close(&client);
+    free(buffer_received);
+
+    return 1;
 }
--- lib/interface.h
+++ lib/interface.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 
+const void *ls(const char *host, unsigned short port);
 const void *file_read(const char *file_name, const char *host, unsigned short port);
 size_t file_write(const char *file_name, const void *buffer, size_t len, short append, const char *host, unsigned short port);
 short file_delete(const char *file_name, const char *host, unsigned short port);
