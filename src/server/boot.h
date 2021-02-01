776af8981427202549c56cf201721f375907ae10
MrPekar98 <martin.pekar@outlook.dk>
2021-02-01 16:14:06 +0100

    Removed debug code.

--- src/server/boot.h
+++ src/server/boot.h
@@ -3,8 +3,7 @@
 
 #define WORKER_DIR "~/.pekar_worker"
 #define MASTER_ADDR "127.0.0.1"
-//#define MASTER_PORT get_port()
-#define MASTER_PORT 55442
+#define MASTER_PORT get_port()
 
 void boot();
 void terminate();
