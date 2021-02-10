24787ee9d107ee0739ea7f7ccd9a2c5085e1aeb1
MrPekar98 <martin.pekar@outlook.dk>
2021-02-10 21:51:21 +0100

    Removed debug code.

--- src/server/boot.h
+++ src/server/boot.h
@@ -1,11 +1,9 @@
 #ifndef BOOT_H
 #define BOOT_H
 
-//#define WORKER_DIR "~/.pekar_worker"
-#define WORKER_DIR "."
+#define WORKER_DIR "~/.pekar_drive"
 #define MASTER_ADDR "127.0.0.1"
-//#define MASTER_PORT get_port()
-#define MASTER_PORT 55442
+#define MASTER_PORT get_port()
 
 short boot();
 void terminate();
