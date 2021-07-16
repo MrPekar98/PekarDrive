0373b956544777b897a1b5eb513a357ab822bd82
MrPekar98 <martin.pekar@outlook.dk>
2021-07-16 11:44:19 +0100

    Initial PP version.

--- include/PP/pp.h
+++ include/PP/pp.h
@@ -1,7 +1,30 @@
-#ifndef pp_h
-#define pp_h
+#ifndef PP_H
+#define PP_H
 
-// Protocol entry file.
-// TODO: Should online define functions for transmission, not server port listening.
+#include <stddef.h>
+#include <comm.h>
+
+typedef struct
+{
+    short error;            // Whether transmission has failed.
+    char *error_message;    // Error message. If no error, this is NULL.
+    size_t bytes;           // Number of transmitted bytes.
+} pp_status;
+
+typedef struct
+{
+    pp_status status;
+    void *buffer;
+} pp_message;
+
+typedef struct
+{
+    conn connection;        // Connection instance.
+} pp_endpoint;
+
+pp_endpoint endpoint_from_address(const char *hostname, unsigned short port);
+pp_endpoint endpoint_from_connection(conn connection);
+pp_status pp_send(pp_endpoint endpoint, const void *data, size_t len);
+pp_message pp_read(pp_endpoint endpoint);
 
 #endif
